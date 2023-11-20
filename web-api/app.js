aa3e6d70e9d9d77237a6a68d380a8b1a6f698e00
nh0ks0ckkut3 <bason1607@gmail.com>
2023-11-20 21:09:15 +0700

    change_link_mongodb_

--- web-api/app.js
+++ web-api/app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 var mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017/demoMD18201', 
+mongoose.connect('mongodb+srv://0osuper:Huyenth0a!@plantsvszombie.tjmo9fh.mongodb.net/', 
   { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('Connected successfully'))
   .catch(err => console.log(err));
