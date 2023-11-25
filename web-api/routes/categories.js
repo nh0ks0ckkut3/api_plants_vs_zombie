286a0b9bd5e7c9ea7ff006e26e995aacc827e9ae
nh0ks0ckkut3 <bason1607@gmail.com>
2023-11-25 13:54:45 +0700

    'returnUrl'

--- web-api/routes/categories.js
+++ web-api/routes/categories.js
@@ -73,4 +73,24 @@ router.delete('/:id', async (req, res, net) =>{
 })
 
 
+
+router.get('/return-url', (req, res) => {
+    const code = req.query.code;
+    const id = req.query.id;
+    const cancel = req.query.cancel === 'true';
+    const status = req.query.status;
+    const orderCode = req.query.orderCode;
+  
+    // Xử lý thông tin từ Return URL
+    if (cancel) {
+      console.log('Thanh toán bị hủy.');
+    } else {
+      console.log(`Thanh toán thành công. Mã đơn hàng: ${orderCode}`);
+    }
+  
+    // Trả về phản hồi cho client (có thể là một trang HTML hoặc JSON response)
+    res.send('Xử lý Return URL thành công!');
+  });
+
+
 module.exports = router;
